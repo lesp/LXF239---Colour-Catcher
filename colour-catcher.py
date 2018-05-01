5d1e59f792e3ee2ddc3523b5f8f5a41aee620d0e
Les Pounder <lespounder@gmail.com>
2018-05-01 17:02:24 +0000

    First commit, working code

---
+++ colour-catcher.py
@@ -0,0 +1,22 @@
+import envirophat, time
+import unicornhat as unicorn
+
+unicorn.set_layout(unicorn.PHAT)
+unicorn.rotation(0)
+unicorn.brightness(0.5)
+envirophat.leds.on()
+
+while True:
+    try:
+        color = (envirophat.light.rgb())
+        for y in range(4):
+            for x in range(8):
+                r = 128
+                g = 0
+                b = 0
+                unicorn.set_pixel(x,y,(color))
+        unicorn.show()
+        time.sleep(1)
+    except KeyboardInterrupt:
+        print("EXIT")
+        break
